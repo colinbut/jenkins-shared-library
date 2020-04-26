2633679964d5b9c80b41210899714d5f3fec3288
Colin But <colin.but@outlook.com>
2020-04-26 19:00:18 +0100

    first shared library global variable function

---
+++ vars/buildJavascriptApp.groovy
@@ -0,0 +1,21 @@
+def call(Map config=[:], Closure body) {
+    node {
+        git url: config.repo
+
+        stage("Install") {
+            sh "npm install"
+        }
+
+        stage("Build") {
+            sh "npm run build"
+        }
+
+        stage("Deploy") {
+            if (config.deploy){
+                echo "Deploying..."
+            }
+        }
+
+        body()
+    }
+}

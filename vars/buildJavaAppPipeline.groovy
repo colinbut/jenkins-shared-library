c6b8c225839fbca76688d12d2f3c421b3e6ee813
Colin But <colin.but@outlook.com>
2020-04-27 18:31:11 +0100

    buildJavaAppPipeline.groovy

---
+++ vars/buildJavaAppPipeline.groovy
@@ -0,0 +1,63 @@
+def call(Map args) {
+    return this
+}
+
+def compile(Map args) {
+    node {
+        stage("Compile") {
+            sh "${args.command}"
+        }
+    }
+    return this
+}
+
+def unitTests(Map args) {
+    node {
+        stage("Unit Tests"){
+            sh "${args.command}"
+        }
+    }
+    return this
+}
+
+def integrationTests(Map args) {
+    node {
+        stage("Integration Tests"){
+            sh "${args.command}"
+        }
+    }
+    return this
+}
+
+def packageArtifact(Map args) {
+    node {
+        stage("Package Artifact"){
+            sh "${args.command}"
+        }
+    }
+    return this
+}
+
+def publishToNexus(Map args) {
+    node {
+        stage("Publish to Nexus") {
+            echo "Publishing to Nexus"
+        }
+    }
+    return this
+}
+
+def publishToArtifactory(Map args) {
+    node {
+        stage("Publish to Artifactory") {
+            echo "Publishing to Artifactory"
+        }
+    }
+    return this
+}
+
+def postBuildSteps(Closure body={}) {
+    node {
+        body()
+    }
+}

f975c04d4fbf8958a89b70c403d35bfd8c91dcdb
Colin But <colin.but@outlook.com>
2020-04-27 18:15:40 +0100

    logging utilities

---
+++ vars/log.groovy
@@ -0,0 +1,15 @@
+def info(message) {
+    echo "INFO: ${message}"
+}
+
+def warning(message) {
+    echo "WARN: ${message}"
+}
+
+def debug(message) {
+    echo "DEBUG: ${message}"
+}
+
+def error(message) {
+    echo "ERROR: ${message}"
+}

6a4379e73dbf692d187e918db9b791c0af34dcff
Colin But <colin.but@outlook.com>
2020-04-27 18:30:21 +0100

    split up assertions to test specific cases in PublishDockerImageSpec.groovy

--- test/vars/PublishDockerImageSpec.groovy
+++ test/vars/PublishDockerImageSpec.groovy
@@ -10,14 +10,23 @@ class PublishDockerImageSpec extends JenkinsPipelineSpecification {
 
     def "Test publish docker image"() {
         setup:
+            def dockerEcr = GroovyMock(DockerEcr.class, global: true)
+            new DockerEcr(publishDockerImage) >> dockerEcr
+        when:
+            publishDockerImage awsCliVersion: 1, microserviceName: "microservice-name"
+        then:
+            1 * dockerEcr.loginToAWSECRDockerRegistry(1)
+            1 * dockerEcr.publishDockerImageToECR("microservice-name")
+    }
+
+    def "Test run closure block after publish docker image"() {
+        setup:
             def body = Mock(Closure)
             def dockerEcr = GroovyMock(DockerEcr.class, global: true)
             new DockerEcr(publishDockerImage) >> dockerEcr
         when:
             publishDockerImage awsCliVersion: 1, microserviceName: "microservice-name", body
         then:
-            1 * dockerEcr.loginToAWSECRDockerRegistry(1)
-            1 * dockerEcr.publishDockerImageToECR("microservice-name")
             1 * body()
     }
 }

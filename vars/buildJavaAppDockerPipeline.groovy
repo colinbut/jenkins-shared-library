a049e8d45b7a8779ad3b97ad752503e1e4f8083e
Colin But <colin.but@outlook.com>
2020-04-28 16:36:11 +0100

    Removed comments

--- vars/buildJavaAppDockerPipeline.groovy
+++ vars/buildJavaAppDockerPipeline.groovy
@@ -4,8 +4,6 @@ import com.mycompany.colinbut.Git
 def call(Map args=[:]) {
     node {
         stage("Checkout") {
-           // git credentialsId: 'github_credentials', url: "https://github.com/colinbut/${args.repo}.git"
-            echo args.repo
            new Git(this).checkout(args.repo)
         }
     }
